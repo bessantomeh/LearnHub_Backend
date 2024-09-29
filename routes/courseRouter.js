00d13141219813cedfdc08774973949efa055be6
Rahaf Nasrallah <nasrallahrahaf7@gmail.com>
2024-09-29 19:22:16 +0300

    remove authentication middleware from the course details endpoint

--- routes/courseRouter.js
+++ routes/courseRouter.js
@@ -8,7 +8,7 @@ const courseRouter = Router();
 courseRouter.post('/createCourse', authorizeUser([roles.admin]), CourseController.createCourse);
 courseRouter.patch('/courses/:courseId', authorizeUser([roles.admin]), CourseController.updateCourse);
 courseRouter.delete('/deleteCourses/:courseId', authorizeUser([roles.admin]), CourseController.deleteCourse);
-courseRouter.get('/coursesDetails/:courseId', authorizeUser([roles.user, roles.admin]), CourseController.getCourseDetails);
+courseRouter.get('/coursesDetails/:courseId', CourseController.getCourseDetails);
 courseRouter.get('/Courses', CourseController.getAllCourses);
 courseRouter.get('/search/subject/:subject',authorizeUser([roles.user, roles.admin]), CourseController.searchCourseBySubject)
 courseRouter.get('/search/title/:title',authorizeUser([roles.user, roles.admin]), CourseController.searchCourseByTitle)
