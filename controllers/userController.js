cf7650e7f4063799e9b8b6b7ebd9eb0dd524259a
Rahaf Nasrallah <nasrallahrahaf7@gmail.com>
2024-10-01 00:00:54 +0300

    adding more admin endpoints

--- controllers/userController.js
+++ controllers/userController.js
@@ -148,6 +148,88 @@ export const deleteUser = async (req, res) => {
     }
   };
   
+  export const updateUserPassword = async (req, res) => {
+    try {
+      const { userId } = req.params;
+      const { password } = req.body;
+      const user = await userModel.findById(userId);
+      
+      if (!user) {
+        return res.status(404).json({ message: 'User not found' });
+      } else {
+        if (!process.env.SALTROUNT || !process.env.EMAILTOKEN) {
+          throw new Error('SALTROUNT | EMAILTOKEN environment variable is not defined.');
+        }
+        const SALTROUNT = parseInt(process.env.SALTROUNT);
+        const hash = bcrypt.hashSync(password, SALTROUNT);
+        const updateUser = await userModel.findByIdAndUpdate(userId, { password: hash });
+        
+        if (!updateUser) {
+          return res.status(500).json({ message: "Failed to update password" });
+        } else {
+          return res.status(200).json({ message: "Password updated successfully" });
+        }
+      }
+    } catch (error) {
+      console.error('Error:', error);
+      return res.status(500).json({ message: 'Server error' });
+    }
+  };
+  
+  export const deleteUserById = async (req, res) => {
+    try {
+      const { userId } = req.params;
+      const user = await userModel.findByIdAndDelete(userId);
+      
+      if (!user) {
+        return res.status(404).json({ message: 'User not found' });
+      }
+      
+      return res.status(200).json({ message: 'Success, User deleted', user });
+    } catch (error) {
+      return res.status(500).json({ message: 'Server error', error: error.message });
+    }
+  };
+  
+  export const updateUserByAdmin = async (req, res) => {
+    try {
+      const { userId } = req.params;
+      const { username, email } = req.body;
+      const updatedUser = await userModel.findByIdAndUpdate(userId, { username, email });
+      
+      if (!updatedUser) {
+        return res.status(404).json({ message: 'User not found' });
+      }
+      
+      return res.status(200).json({ message: 'User updated successfully', updatedUser });
+    } catch (error) {
+      return res.status(500).json({ message: 'Server error' });
+    }
+  };
+  
+  export const createUser = async (req, res, next) => {
+    try {
+      const { username, email, password } = req.body;
+      
+      if (!process.env.SALTROUNT || !process.env.EMAILTOKEN) {
+        throw new Error('SALTROUNDS or EMAILTOKEN environment variable is not defined.');
+      }
+      
+      const existingUser = await userModel.findOne({ email });
+      if (existingUser) {
+        return next(Object.assign(new Error('Email already exists'), { cause: 409 }));
+      } else {
+        const saltRounds = parseInt(process.env.SALTROUNT);
+        const hashedPassword = await bcrypt.hash(password, saltRounds);
+        const newUser = new userModel({ username, email, password: hashedPassword, confirmEmail: true });
+        const token = jwt.sign({ id: newUser._id }, process.env.EMAILTOKEN, { expiresIn: '1h' });
+        await newUser.save();
+        return res.status(201).json({ message: 'User created successfully', user: token });
+      }
+    } catch (error) {
+      return next(Object.assign(new Error('Server error'), { cause: 500 }));
+    }
+  };
   
   
   
--- routes/userRouter.js
+++ routes/userRouter.js
@@ -10,13 +10,13 @@ userRouter.patch('/updatePassword',authorizeUser([roles.user,roles.admin]),userC
 userRouter.put("/updateUser",authorizeUser([roles.user,roles.admin]), userController.updateUser);
 userRouter.get('/allUsers',authorizeUser([roles.admin]),userController.getAllUsers)
 userRouter.delete('/delete',authorizeUser([roles.admin,roles.user]),userController.deleteUser)
-//userRouter.post('/admin/createUser',authorizeUser([roles.admin]),userController.createUser)
-//userRouter.delete('/admin/user/:id', authorizeUser([roles.admin]), userController.deleteUser);
-//userRouter.put('/admin/user/:id/password', authorizeUser([roles.admin]),userController.updateUserPassword);
-userRouter.get('/admin/user/:userId',authorizeUser([roles.admin]),userController.getUserById);
-//userRouter.put('admin/updateUser/:userId',authorizeUser([roles.admin]),userController.adminUpdateUser)
-userRouter.get('/admin/users/search/name/:username',authorizeUser([roles.admin]),userController.searchUserByName)
-userRouter.get('/admin/users/search/email/:email',authorizeUser([roles.admin]),userController.searchUserByEmail)
+userRouter.get('/:userId',authorizeUser([roles.admin]),userController.getUserById);
+userRouter.get('/users/search/name/:username',authorizeUser([roles.admin]),userController.searchUserByName)
+userRouter.get('/users/search/email/:email',authorizeUser([roles.admin]),userController.searchUserByEmail)
+router.post('/createUser',authorizeUser([roles.admin]), userController.createUser)
+router.delete('/deleteUser/:userId',authorizeUser([roles.admin]),userController.deleteUserById)
+router.put("/updateUserPassword/:userId",authorizeUser([roles.admin]), userController.updateUserPassword);
+router.put("/updateUserbyAdmin/:userId",authorizeUser([roles.admin]), userController.updateUserByAdmin);
 
 
 
