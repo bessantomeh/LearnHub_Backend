51eaa993335369da0dca816a1e597cd1302edebe
Rahaf Nasrallah <nasrallahrahaf7@gmail.com>
2024-10-01 00:08:01 +0300

    adding more admin endpoints

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -10,11 +10,11 @@ userRouter.patch('/updatePassword',authorizeUser([roles.user,roles.admin]),userC
 userRouter.put("/updateUser",authorizeUser([roles.user,roles.admin]), userController.updateUser);
 userRouter.get('/allUsers',authorizeUser([roles.admin]),userController.getAllUsers)
 userRouter.delete('/delete',authorizeUser([roles.admin,roles.user]),userController.deleteUser)
-//userRouter.post('/admin/createUser',authorizeUser([roles.admin]),userController.createUser)
-//userRouter.delete('/admin/user/:id', authorizeUser([roles.admin]), userController.deleteUser);
-//userRouter.put('/admin/user/:id/password', authorizeUser([roles.admin]),userController.updateUserPassword);
+userRouter.post('/admin/createUser',authorizeUser([roles.admin]),userController.createUser)
+userRouter.delete('/admin/user/:userId', authorizeUser([roles.admin]), userController.deleteUserById);
+userRouter.put('/admin/user/password/:userID', authorizeUser([roles.admin]),userController.updateUserPassword);
 userRouter.get('/admin/user/:userId',authorizeUser([roles.admin]),userController.getUserById);
-//userRouter.put('admin/updateUser/:userId',authorizeUser([roles.admin]),userController.adminUpdateUser)
+userRouter.put('admin/updateUser/:userId',authorizeUser([roles.admin]),userController.updateUserByAdmin)
 userRouter.get('/admin/users/search/name/:username',authorizeUser([roles.admin]),userController.searchUserByName)
 userRouter.get('/admin/users/search/email/:email',authorizeUser([roles.admin]),userController.searchUserByEmail)
 userRouter.post('/signOut',authorizeUser([roles.admin,roles.user]),userController.signOut)
