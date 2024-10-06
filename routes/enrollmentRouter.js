407df2ada9b9766c54725e5ce4dc1a008b693529
bessan-tomeh <101426292+bessantomeh@users.noreply.github.com>
2024-10-06 22:57:49 +0300

    Update enrollmentRouter.js

--- routes/enrollmentRouter.js
+++ routes/enrollmentRouter.js
@@ -2,13 +2,14 @@ import { roles } from '../services/role.js';
 import * as EnrollmentController from '../controllers/EnrollmentController.js'; 
 import {authorizeUser} from "../middleware/authentication.js";
 import { Router } from "express";
-
+import { validation } from './../middleware/requestValidation.js';
+import * as validators from './../validation/courseValidation.js';
 
 const enrollRouter = Router()
 
-enrollRouter.post('/enroll',authorizeUser([roles.user]), EnrollmentController.enrollInCourse)
+enrollRouter.post('/enroll',authorizeUser([roles.user]),validation(validators.enroll), EnrollmentController.enrollInCourse)
 enrollRouter.get('/courses/user',authorizeUser([roles.user]), EnrollmentController.listUserCourses)
-enrollRouter.post('/unenroll',authorizeUser([roles.user]), EnrollmentController.unenrollFromCourse)
+enrollRouter.post('/unenroll',authorizeUser([roles.user]),validation(validators.enroll), EnrollmentController.unenrollFromCourse)
 
 
 export default enrollRouter;
