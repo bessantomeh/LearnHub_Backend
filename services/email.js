c5cf254d7ead6a1a080c736ae02c486e937887af
Rahaf Nasrallah <nasrallahrahaf7@gmail.com>
2024-10-06 21:29:41 +0300

    update on authController

--- services/email.js
+++ services/email.js
@@ -5,42 +5,33 @@ import nodemailer from "nodemailer";
   of the sent email or throws an error if sending fails.
  */
 
-  class EmailService {
-    constructor() {
-      this.transporter = this.createTransporter(); // Create transporter instance when EmailService is instantiated
-    }
-  
-    createTransporter() {
-      return nodemailer.createTransport({
-        host: "smtp.gmail.com",
-        port: 587,
-        secure: false,
-        service: "gmail",
-        auth: {
-          user: process.env.SENDER_EMAIL,
-          pass: process.env.SENDER_EMAIL_PASSWORD,
-        },
-      });
-    }
-  
-    async sendEmail(dest, subject, message) {
-      try {
-        const mailOptions = {
-          from: `"LearnHub" <${process.env.SENDER_EMAIL}>`,
-          to: [dest],
-          subject: subject,
-          html: message,
-        };
-  
-        const info = await this.transporter.sendMail(mailOptions);
-        console.log("Email sent:", info.response);
-        return info;
-      } catch (error) {
-        console.error("Error sending email:", error);
-        throw error;
-      }
-    }
+async function sendEmail(dest, subject, message) {
+  try {
+    let transporter = nodemailer.createTransport({
+      host: "smtp.gmail.com",  
+      port: 587,               
+      secure: false,           
+      service: "gmail",        
+      auth: {
+        user: process.env.SENDER_EMAIL,         
+        pass: process.env.SENDER_EMAIL_PASSWORD, 
+      },
+    });
+
+    const mailOptions = {
+      from: `"LearnHub" <${process.env.SENDER_EMAIL}>`,  
+      to: [dest],                                      
+      subject: subject,                                
+      html: message,                                  
+    };
+
+    const info = await transporter.sendMail(mailOptions);
+    console.log("Email sent:", info.response);
+    return info;  
+  } catch (error) {
+    console.error("Error sending email:", error);
+    throw error;  
   }
-  
-  export default EmailService;
-  
+}
+
+export default sendEmail;
