df93afad56a69285ea9c61de52aba49683970ec4
Rahaf Nasrallah <nasrallahrahaf7@gmail.com>
2024-10-07 17:14:43 +0300

    optimize sending mail

--- services/email.js
+++ services/email.js
@@ -1,28 +1,33 @@
 import nodemailer from "nodemailer";
-
 /*
   Sends an email using nodemailer, Resolves with the info
   of the sent email or throws an error if sending fails.
  */
 
+
+// Factory method for creating transporters
+function EmailTransporterFactory() {
+  return nodemailer.createTransport({
+    host: "smtp.gmail.com",  
+    port: 587,               
+    secure: false,           
+    service: "gmail",        
+    auth: {
+      user: process.env.SENDER_EMAIL,         
+      pass: process.env.SENDER_EMAIL_PASSWORD, 
+    },
+  });
+}
+
 async function sendEmail(dest, subject, message) {
   try {
-    let transporter = nodemailer.createTransport({
-      host: "smtp.gmail.com",  
-      port: 587,               
-      secure: false,           
-      service: "gmail",        
-      auth: {
-        user: process.env.SENDER_EMAIL,         
-        pass: process.env.SENDER_EMAIL_PASSWORD, 
-      },
-    });
+    const transporter = EmailTransporterFactory();
 
     const mailOptions = {
       from: `"LearnHub" <${process.env.SENDER_EMAIL}>`,  
       to: [dest],                                      
       subject: subject,                                
-      html: message,                                  
+      html: message,                                   
     };
 
     const info = await transporter.sendMail(mailOptions);
